2d8d1a80f4247088cc1b9dc8ec01a745e4406b15
aeweber <andres.weber@kellogg.northwestern.edu>
2020-02-25 06:40:06 +0000

    3rd part

--- app.rb
+++ app.rb
@@ -8,7 +8,8 @@ before { puts "Parameters: #{params}" }
 
 # enter your Dark Sky API key here
 ForecastIO.api_key = "c54805539b0bda883ac19578b12383dc"
-url = "https://newsapi.org/v2/top-headlines?country=us&apiKey=2b24fb4f120746c5bd6a81a21b2f8643"
+url = "https://newsapi.org/v2/top-headlines?country=us&pageSize=7&apiKey=2b24fb4f120746c5bd6a81a21b2f8643" 
+# page size = 7, to make the web app more user friendly and doesn't show too many news 
 
 get "/" do
   # show a view that asks for the location
@@ -23,6 +24,7 @@ get "/news" do
     lat_lng = results.first.coordinates
     @lat = lat_lng[0]
     @lng = lat_lng[1]
+    @lat_lng = "#{@lat},#{@lng}"
     @forecast = ForecastIO.forecast(@lat,@lng).to_hash
     @current_temperature = @forecast["currently"]["temperature"]
     @current_conditions = @forecast["currently"]["summary"]
